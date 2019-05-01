5057497b20e9ebe3e9fb471573c3bb736ba6dae5
ByungChan Son <byungChan Son@test.com>
2019-05-01 15:49:16 +0900

    5월 1일 수업

---
+++ C_Study/1_CLI/2_printf/4_print_various_const.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+	//printf(5); warning , 수행시 Runtime Error
+	//printf(0); warning , 수행시 Runtime Error
+	
+	//printf(3.14); compile error
+	//printf('A'); warning , 수행시 Runtime Error
+}
