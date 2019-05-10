230ff3d5df68568a9387cdb005c4687268d4021f
ByungChan Son <byungChan Son@test.com>
2019-05-10 17:26:39 +0900

    5월 10일 수업

--- C_Study/1_CLI/2_control_repetition/3_for/q1.c
+++ C_Study/1_CLI/2_control_repetition/3_for/q1.c
@@ -5,7 +5,7 @@ void main(){
 	int num2=0;
 	int total=0;
 
-	printf("두 정수를 입력하세요 (앞의 정수보다 뒤의 정수가커야됨 ex: 3 6): ");
+	printf("두 정수를 입력하세요 (앞의 정수보다 뒤의 정수가 커야됨 ex:3 6): ");
 	scanf("%d %d",&num1, &num2);
 
 	for(; num1!=num2+1; num1++){
