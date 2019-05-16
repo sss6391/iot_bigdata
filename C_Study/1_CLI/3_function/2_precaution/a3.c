05cbc5ef2f32b1513ac2bc3284e21da6c6e46101
ByungChan Son <byungChan Son@test.com>
2019-05-16 09:26:04 +0900

    a

--- C_Study/1_CLI/3_function/2_precaution/a3.c
+++ C_Study/1_CLI/3_function/2_precaution/a3.c
@@ -11,13 +11,12 @@ void main(){
 	scanf("%d", &input_num);
 	
 	for(; num <= input_num ; num++){
-			max=1;
-		while(limit < 5){
+		max=1;
+		while(max < 1000000){
 			if( (num / max) % 10 == 3){
 				count++;
-				limit++;
-				max = max * 10;
 			}
+		max = max * 10;		
 		}
 	}
 	printf("%d", count);
